f3860645878ab89c3a7eeb9f8755940294ef1c66
deepak_pawar1 <deepak_pawar1@PS8721.persistent.co.in>
2017-01-05 17:21:52 +0530

    Sample class edited from second location

--- Test/src/main/java/com/example/Sample.java
+++ Test/src/main/java/com/example/Sample.java
@@ -4,5 +4,7 @@ public class Sample {
 
 	public void show() {
 		System.out.println("This is sample project for GIT");
+		System.out.println("This is second line");
+		System.out.println("This is second location");
 	}
 }
